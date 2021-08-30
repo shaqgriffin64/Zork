a059261063ab4587f908aee7a06a5dae65e4d06f
Shaquille Griffin <55068788+shaqgriffin64@users.noreply.github.com>
2021-08-30 14:47:52 -0400

    Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,21 @@ namespace ZorkGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \n A rubber may saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
