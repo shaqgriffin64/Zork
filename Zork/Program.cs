dcde1e257bbe89ac4f8ab244c9a0f3068d5d5cbb
Shaquille Griffin <55068788+shaqgriffin64@users.noreply.github.com>
2021-09-01 14:48:48 -0400

    Zork 1.2 Finished (for real this time)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -12,7 +12,6 @@ namespace ZorkGame
             Commands command = ToCommand(inputString.Trim().ToUpper());
             Console.WriteLine(command);
         }
-
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
