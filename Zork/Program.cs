ecfa6840204ff7ca33a7baa550a05e8d8f191441
shaqgriffin64 <55068788+shaqgriffin64@users.noreply.github.com>
2021-09-22 11:41:41 -0400

    Zork 2.3 + work on Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -24,84 +24,67 @@ namespace ZorkGame
 
                 Console.WriteLine(CurrentRoom);
 ;                Console.Write("\n> ");
-                command = ToCommand(Console.ReadLine().Trim().ToCommand());
+                command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
+                //string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
                         command = Commands.QUIT;
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
                         command = Commands.LOOK;
-                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door. \n";
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door. \n");
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
-                        if (Move(command))
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        if (Move(command) == false)
                         {
-                            outputString = $"You moved {command}. \n";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut. \n";
+                            Console.WriteLine("The way is shut!");
                         }
                         break;
 
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        //outputString = Move(command) ? $"You moved {command}." : "The way is shut.";
-                        if (Move(command))
-                        {
-                            outputString = $"You moved {command}. \n";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut. \n";
-                        }
-                        break;
                     default:
-                        command = Commands.UNKNOWN;
-                        outputString = "Unknown command. \n";
+                        Console.WriteLine("Unknown command.");
                         break;
                 };
-                Console.WriteLine(outputString);
             }
         }
 
         private static bool Move(Commands command)
         {
-            bool didMove = false;
-
+            bool isValidMove = true;
             switch (command)
             {
-                case Commands.NORTH when LocationRow < Rooms.GetLength(0) - 1:
-                    LocationRow++;
-                    didMove = true;
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
                     break;
-                case Commands.SOUTH when LocationRow > 0:
-                    LocationRow--;
-                    didMove = true;
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
                     break;
 
-                case Commands.EAST when LocationColumn < Rooms.GetLength(1) - 1:
-                    LocationColumn++;
-                    didMove = true;
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
                     break;
-                case Commands.WEST when LocationColumn > 0:
-                    LocationColumn--;
-                    didMove = true;
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
+                    break;
+
+                default:
+                    isValidMove = false;
                     break;
             }
-            return didMove;
+            return isValidMove;
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        //private static bool IsDirection(Commands command) => Directions.Contains(command);
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
 
         private static void InitializeRoomDescriptions()
         {
