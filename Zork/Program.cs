09cdde47013612bbb7b74c59a2a75db3e203d7c9
shaqgriffin64 <55068788+shaqgriffin64@users.noreply.github.com>
2021-10-06 22:02:25 -0400

    Revert "Zork 4.1 Finished"
    This reverts commit 96a54441a1d65e558fd3966b2f3a6d55e8ebefee.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,23 +4,7 @@ namespace ZorkGame
 {
     public class Program
     {
-<<<<<<< Updated upstream
          static void Main(string[] args)
-=======
-        private static readonly Dictionary<string, Room> RoomMap;
-
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-	            {
-                    RoomMap[room.Name] = room;
-	            }
-
-        }
-
-    public static Room CurrentRoom
->>>>>>> Stashed changes
         {
             const string defaultGameFilename = "Zork.json";
             string gameFilename = args.Length > 0 ? args[(int)CommandLineArguements.GameFilename] : defaultGameFilename;
