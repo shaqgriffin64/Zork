42adae1081b4aff4692fa84c143e828de5b52a82
shaqgriffin64 <55068788+shaqgriffin64@users.noreply.github.com>
2021-11-04 23:50:23 -0400

    Actually added Control prefab this time

--- ZorkBuilder/Controls/EquippedItemControl.Designer.cs
+++ ZorkBuilder/Controls/EquippedItemControl.Designer.cs
@@ -35,16 +35,21 @@ namespace ZorkBuilder.Controls
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 26);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(1, 1);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(120, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // comboBox1
             // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(17, 52);
+            this.comboBox1.Location = new System.Drawing.Point(0, 27);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
@@ -56,6 +61,7 @@ namespace ZorkBuilder.Controls
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Name = "EquippedItemControl";
+            this.Size = new System.Drawing.Size(122, 49);
             this.ResumeLayout(false);
             this.PerformLayout();
 
